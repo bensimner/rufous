1549d928cb86b1f28ff13e84251437ad67202baa
Ben Simner <ben.simner@cl.cam.ac.uk>
2020-05-30 17:36:59 +0100

    Error message for DUG generation without Shadow
    To generate a DUG requires a Shadow to be defined.
    This causes a readable error message if the user tries
    to generate a DUG without having defined a Shadow.

--- rufous/Test/Rufous/Generate.hs
+++ rufous/Test/Rufous/Generate.hs
@@ -16,9 +16,18 @@ import qualified Test.Rufous.DUG as D
 import Test.Rufous.Internal.Generate.Types
 import Test.Rufous.Internal.Generate.Core
 
+precheck :: Opt.RufousOptions -> S.Signature -> P.Profile -> IO ()
+precheck _ s _ = do
+   case s^.S.shadowImpl of
+      Just _  -> return ()
+      Nothing -> fail $
+       "Rufous: could not find Shadow implementation for " ++ s^.S.signatureADTName
+       ++ ". A Shadow is required for DUG generation."
+
 -- | Generates a 'DUG' which conforms to a given Profile.
 generateDUG :: Opt.RufousOptions -> S.Signature -> P.Profile -> IO D.DUG
 generateDUG o s p = do
+      precheck o s p
       name <- freshName
       let size = p^.P.size
       let (a, s') = runState (build size) (emptyGenSt o s p name)
