02f433c29a69500bc7301e2ef00269cd296203e9
Ben Simner <ben.simner@cl.cam.ac.uk>
2020-06-17 13:22:26 +0100

    Keep progress bar below scrolling text

--- rufous/Test/Rufous/Internal/Logger.hs
+++ rufous/Test/Rufous/Internal/Logger.hs
@@ -1,5 +1,4 @@
---
-
+-- | Internal logging module
 module Test.Rufous.Internal.Logger
     ( info
     , log
@@ -74,52 +73,61 @@ debugTrace :: RufousOptions -> String -> IO ()
 debugTrace opts msg = doIf Opt.debug opts (debugLn msg)
 
 traceLn :: String -> IO ()
-traceLn s = last s `seq` mapM_ (hPutStrLn stderr) (map ("[ INFO] " ++) (lines s))
+traceLn s = last s `seq` mapM_ debugPutLn (map ("[ INFO] " ++) (lines s))
 
 logLn :: String -> IO ()
-logLn s = last s `seq` mapM_ (hPutStrLn stderr) (map ("[  LOG] " ++) (lines s))
+logLn s = last s `seq` mapM_ debugPutLn (map ("[  LOG] " ++) (lines s))
 
 debugLn :: String -> IO ()
-debugLn s = last s `seq` mapM_ (hPutStrLn stderr) (map ("[DEBUG] " ++) (lines s))
+debugLn s = last s `seq` mapM_ debugPutLn (map ("[DEBUG] " ++) (lines s))
+
+debugPutLn :: String -> IO ()
+debugPutLn s = do
+    hPutStr stderr ("\r" ++ s ++ "\n")
+    refreshProgress
 
 {- Progress Bar -}
-_progressBar :: Ref.IORef (Int, Int, String)
+_progressBar :: Ref.IORef (Maybe (Int, Int, String))
 {-# NOINLINE _progressBar #-}
-_progressBar = unsafePerformIO $ Ref.newIORef (0,0,"")
+_progressBar = unsafePerformIO $ Ref.newIORef Nothing
 
 initProgress :: Int -> IO ()
 initProgress n =
-    Ref.writeIORef _progressBar (0, n, "")
+    Ref.writeIORef _progressBar (Just (0, n, ""))
 
 initProgressWithMsg :: Int -> String -> IO ()
 initProgressWithMsg n msg = do
-    Ref.writeIORef _progressBar (0, n, msg)
+    Ref.writeIORef _progressBar (Just (0, n, msg))
 
 refreshProgress :: IO ()
 refreshProgress = do
-    (i, maxi, msg) <- Ref.readIORef _progressBar
-    let perc :: Float; perc = fromIntegral i / fromIntegral maxi
-    let percInt :: Int; percInt = round $ 100 * perc
-    let len = round $ 20 * perc
-    hPutStr stderr $ "\r[" ++ replicate len '#' ++ replicate (20-len) ' ' ++ "] " ++ show percInt ++ "% " ++ msg
-    hFlush stderr
+    pg <- Ref.readIORef _progressBar
+    case pg of
+        Just (i, maxi, msg) -> do
+            let perc :: Float; perc = fromIntegral i / fromIntegral maxi
+            let percInt :: Int; percInt = round $ 100 * perc
+            let len = round $ 20 * perc
+            hPutStr stderr $ "\r[" ++ replicate len '#' ++ replicate (20-len) ' ' ++ "] " ++ show percInt ++ "% " ++ msg
+            hFlush stderr
+            return ()
+        Nothing -> return ()
 
 updateProgress :: Int -> IO ()
 updateProgress di = do
-    (c, maxi, msg) <- Ref.readIORef _progressBar
+    Just (c, maxi, msg) <- Ref.readIORef _progressBar
     let i = c+di
-    Ref.writeIORef _progressBar (i, maxi, msg)
+    Ref.writeIORef _progressBar (Just (i, maxi, msg))
     refreshProgress
 
 updateProgressMsg :: String -> IO ()
 updateProgressMsg m = do
-    (c, maxi, _) <- Ref.readIORef _progressBar
-    Ref.writeIORef _progressBar (c, maxi, m)
+    Just (c, maxi, _) <- Ref.readIORef _progressBar
+    Ref.writeIORef _progressBar (Just (c, maxi, m))
     refreshProgress
 
 endProgress :: IO ()
 endProgress = do
-    (_, maxi, msg) <- Ref.readIORef _progressBar
-    Ref.writeIORef _progressBar (maxi, maxi, msg)
+    Just (_, maxi, msg) <- Ref.readIORef _progressBar
+    Ref.writeIORef _progressBar (Just (maxi, maxi, msg))
     refreshProgress
     hPutStrLn stderr ""
